77544dc1e30b59b032ab4e38015316a1c193f468
louis <louisreypro@yahoo.com>
2023-02-15 11:54:25 +0100

    Fetch request and transform type CDTask in Task

--- Tasks/Management/Storage/CoreDataStorage.swift
+++ Tasks/Management/Storage/CoreDataStorage.swift
@@ -25,9 +25,38 @@ class CoreDataStorage {
        return persistentContainer.viewContext
     }
     
+    /// Pour récupérer la liste des taches depuis la base de donnée
     func fetchTaskList() -> [Task] {
+        let taskList: [Task]
         let fetchRequest: NSFetchRequest<CDTask> = CDTask.fetchRequest()
+        /// Transformer la requête en résultat
+        if let rawTaskList = try? context.fetch(fetchRequest) {
+            
+            /// Transformer objet de type CDTask -> Task
+            taskList = rawTaskList.compactMap({ (rawTask: CDTask) -> Task? in
+                
+                Task(fromCoreDataObject: rawTask)
+            })
+            
+        } else {
+            taskList = []
+        }
+        /// Il charge les données et retourne un résultat
         
-        return []
+        return taskList
+    }
+}
+
+extension Task {
+    
+    init?(fromCoreDataObject coreDataObject: CDTask) {
+        
+        guard let id = coreDataObject.id,
+        let name = coreDataObject.name
+        else { return nil }
+        
+        self.id = id
+        self.name = name
+        self.isDone = coreDataObject.isDone
     }
 }
