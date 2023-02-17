d28a66dad498da5240810082ee34ad2e4ef92e52
Louis Rey <louisreypro@yahoo.com>
2023-02-17 13:17:46 +0100

    modify file

--- Tasks/UI/TaskCell.swift
+++ Tasks/UI/TaskCell.swift
@@ -8,20 +8,25 @@
 import SwiftUI
 
 struct TaskCell: View {
-    
-    @State var taskManager = TaskManager()
-    @State var task: Task
+    let task: Task
     
     var body: some View {
-        VStack {
-            Text(taskManager.taskList.description)
+        HStack {
             Text(task.name)
-        }
+                .font(.title2)
+            Spacer()
+            Image(systemName: task.isDone ? "checkmark.circle" : "circle")
+                .font(.title)
+        }.padding()
     }
 }
 
 struct TaskCell_Previews: PreviewProvider {
     static var previews: some View {
-        TaskCell(task: Task(name: "test"))
+        Group {
+            TaskCell(task: Task(name: "test"))
+            TaskCell(task: Task(name: "test", isDone: true))
+        }
+        .previewLayout(.fixed(width: 500.0, height: 100.0))
     }
 }
